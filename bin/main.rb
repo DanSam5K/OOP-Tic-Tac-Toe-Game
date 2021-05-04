a104e3d3b270df5dc0ad50fe3f03fde4d7a312f9
Mario Rodriguez <mariordgez@hotmail.com>
2021-05-03 22:27:22 -0500

    Added player name input to main rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,26 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+player_1=""
+player_2=""
+puts "Welcome to Ruby Tic-Tac-Toe! "
+while player_1==""
+puts "Enter player 1 name: "
+player_1 = gets.chomp
+if player_1==""
+  puts "Please enter a name for Player 1 (press enter to continue)"
+  gets.chomp
+  system("clear")
+end
+end
+while player_2==""
+puts "Enter player 2 name: "
+player_2 = gets.chomp
+if player_2==""
+  puts "Please enter a name for Player 2 (press enter to continue)"
+  gets.chomp
+  system("clear")
+end
+end
+puts "#{player_1} will use X while #{player_2} will use O."
+
+puts "Lets start! (Press enter to continue)"
+gets.chomp
