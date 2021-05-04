459609635813c655df5cc33ea55b4a406f8ed43c
Mario Rodriguez <mariordgez@hotmail.com>
2021-05-03 22:29:08 -0500

    Added turns and winner section

--- bin/main.rb
+++ bin/main.rb
@@ -39,3 +39,42 @@ while turns<6
   puts "| 7 | 8 | 9 |"
   puts "+---+---+---+"
   
+  if current_player==player_1  
+    puts "Its #{current_player} turn"
+    puts "Please select an available cell: "
+    player_1_cell=gets.chomp.to_i
+    #if player inputs anything other than a number it will be equal to 0 therefore outside of the if condition
+    if player_1_cell>0 &&  player_1_cell<10
+    current_player=player_2
+    turns+=1
+    
+    else
+      puts "Invalid move, please enter a number from 1-9"
+      gets.chomp
+    end
+    else
+    puts "Its #{current_player} turn"
+    puts "Please select an available cell: "
+    player_2_cell=gets.chomp.to_i
+    if player_2_cell>0  &&  player_2_cell<10
+    current_player=player_1
+    turns+=1
+    else
+      puts "Invalid move, please enter a number from 1-9"
+      gets.chomp
+    end
+    end
+    if turns==6
+      #for now the program chooses a random winner based on the results of rand
+      winner=rand(1..2)
+      if winner==1
+        puts "#{player_1} is the winner!!"
+        gets.chomp
+      else
+        puts "#{player_2} is the winner!!"
+        gets.chomp
+      end
+    end 
+    system("clear")
+    end
+    
