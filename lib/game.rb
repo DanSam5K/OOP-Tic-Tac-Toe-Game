8ba01ac016894f5b5b6bae4c37a7a4134aef79ee
Daniel Samuel <dansam5000@gmail.com>
2021-05-04 22:57:54 +0100

    Add initialize and add win combination

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,11 @@
 require './player.rb'
 class Game
-    def initialize(player1, player2)
-        @player1 = Player.new(self, "X")
-        @player2 = Player.new(self, "O")
+
+    def initialize()
+        @board = Player.new(9, " ")
     end
-    def winner?
-        win_condition = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
-        @player1.store_selection
-        for i in win_condition do
-              
-        end 
-    end
+    WIN_COMBINATION = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
+    
+
+   
 end
