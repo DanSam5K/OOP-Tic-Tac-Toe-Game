753e071f86625e67dcb905b00f1f0556a06c7404
Daniel Samuel <dansam5000@gmail.com>
2021-05-04 11:40:37 +0100

    modify variables names

--- bin/main.rb
+++ bin/main.rb
@@ -1,33 +1,33 @@
 #!/usr/bin/env ruby
-player1 = ''
-player2 = ''
+first_player = ''
+second_player = ''
 puts 'Welcome to Ruby Tic-Tac-Toe! '
-while player1 == ''
+while first_player == ''
   puts 'Enter player 1 name: '
-  player1 = gets.chomp
-  next unless player1 == ''
+  first_player = gets.chomp
+  next unless first_player == ''
 
   puts 'Please enter a name for Player 1 (press enter to continue)'
   gets.chomp
   system('clear')
 end
-while player2 == ''
+while second_player == ''
   puts 'Enter player 2 name: '
-  player2 = gets.chomp
-  next unless player2 == ''
+  second_player = gets.chomp
+  next unless second_player == ''
 
   puts 'Please enter a name for Player 2 (press enter to continue)'
   gets.chomp
   system('clear')
 end
-puts "#{player1} will use X while #{player2} will use O."
+puts "#{first_player} will use X while #{second_player} will use O."
 
 puts 'Lets start! (Press enter to continue)'
 gets.chomp
 system('clear')
 
 turns = 0
-current_player = player1
+current_player = first_player
 
 while turns < 6
   # This is the current board, we are currently working on making a better looking board
@@ -41,11 +41,11 @@ while turns < 6
 
   puts "Its #{current_player} turn"
   puts 'Please select an available cell: '
-  if current_player == player1
+  if current_player == first_player
     player_1_cell = gets.chomp.to_i
     # if player inputs anything other than a number it will be equal to 0 therefore outside of the if condition
     if player_1_cell.positive? && player_1_cell < 10
-      current_player = player2
+      current_player = second_player
       turns += 1
 
     else
@@ -55,7 +55,7 @@ while turns < 6
   else
     player_2_cell = gets.chomp.to_i
     if player_2_cell.positive? && player_2_cell < 10
-      current_player = player1
+      current_player = first_player
       turns += 1
     else
       puts 'Invalid move, please enter a number from 1-9'
@@ -66,9 +66,9 @@ while turns < 6
     # for now the program chooses a random winner based on the results of rand
     winner = rand(1..2)
     if winner == 1
-      puts "#{player1} is the winner!!"
+      puts "#{first_player} is the winner!!"
     else
-      puts "#{player2} is the winner!!"
+      puts "#{second_player} is the winner!!"
     end
     gets.chomp
   end
