0d6cef7a5566c2ca8c721b92ab657cec58bf808f
Daniel Samuel <dansam5000@gmail.com>
2021-05-04 18:59:24 +0100

    Add some game logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,14 @@
+require './player.rb'
 class Game
-
+    def initialize(player1, player2)
+        @player1 = Player.new(self, "X")
+        @player2 = Player.new(self, "O")
+    end
+    def winner?
+        win_condition = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
+        @player1.store_selection
+        for i in win_condition do
+              
+        end 
+    end
 end
