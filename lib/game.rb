2ad6463841593a77c4f707d7f626215d8e67b2c5
Daniel Samuel <dansam5000@gmail.com>
2021-05-04 23:10:45 +0100

    Add display board

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,16 @@
 require './player.rb'
 class Game
-
-    def initialize()
-        @board = Player.new(9, " ")
-    end
-    WIN_COMBINATION = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
-    
-
+  def initialize()
+    @board = Player.new(9, " ")
+  end
+  WIN_COMBINATION = [[1, 2, 3], [1, 4, 7], [2, 5, 8], [3, 6, 9], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7]]
+  
+  def display_board
+    puts "{@board[1]} | {@board[2]} | {@board[3]}"
+    puts "------------"
+    puts "{@board[4]} | {@board[5]} | {@board[5]}"
+    puts "------------"
+    puts "{@board[7]} | {@board[8]} | {@board[9]}"
+  end
    
 end
