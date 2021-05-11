ae13e5866e89febf14004f96875b20a5a4663e04
YuvrajM108 <yuvrajmanoo1@gmail.com>
2021-05-11 12:46:11 -0400

    Modify #choices test case for modified board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,8 +3,9 @@ require './lib/game'
 describe Game do
     inplay = Game.new()
     describe "#choices" do
-        it "should display choices from 1-9" do
-            expect(inplay.choices).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        it "should display available choices" do
+            inplay.move(1, "X")
+            expect(inplay.choices).to eql([1, "X", 3, 4, 5, 6, 7, 8, 9])
         end
     end
 end
