69e85583f561c45a1961068fe8c2298ce76c81b2
Daniel Samuel <dansam5000@gmail.com>
2021-05-11 22:50:52 +0100

    Add Player class test cases

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require "./lib/player"
+
+describe Player do
+  available_player = Player.new("player_name", "X")
+  describe "#choice" do
+    it "Should add selected number and return selection array" do
+        expect(available_player.choice(0)).to eql([0])
+    end    
+  end
+end
